3f379509a08062efb4ac6e9a3b2eba87e60e066a
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-17 15:02:57 -0400

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return items[0] + " and " + items[1]
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
